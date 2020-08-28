4fcfc33f92dd726e2b2e70b009345d3b28745da1
Powaga, Michał (EXT) <michal.powaga@skanska.pl>
2020-08-28 22:49:26 +0200

    Two containers ready for AWS deployment

--- fibweb/src/App.js
+++ fibweb/src/App.js
@@ -7,10 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is MY React Application!
-        </p>
-        <p>React app running in nginx container.</p>
+        <p>This is MY React Application!</p>
+        <p>Accessed via nginx proxy with live update if in dev dockerfile is used.</p>
+        <p>I'm going to create multiple docker-compose.yml files (dev and prod). WORKS FINE!</p>
+        <p>...but NOT everytime ;-(!</p>
       </header>
     </div>
   );
